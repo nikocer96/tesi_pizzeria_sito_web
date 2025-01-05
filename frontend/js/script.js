97c86875076135d722a01f78a8db7ac300c4ec19
nicocer96 <you@example.comnikocerone96@gmail.com>
2025-01-05 16:41:17 +0100

    Added index, scopri and prenota style responsive

---
+++ frontend/js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    const myLinks = document.getElementById("myLinks");
+    if (myLinks.style.display === "block") {
+      myLinks.style.display = "none";
+    } else {
+      myLinks.style.display = "block";
+    }
+  }
